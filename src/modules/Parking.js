1a2e0c853ae9d011712ca2460926ae92b8bb3242
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-11-20 16:05:03 -0400

    parking page - layout

--- src/modules/Parking.js
+++ src/modules/Parking.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+
 const Parking = () => (
-  <div>Parking page</div>
+  <div>
+    <div><h1>Parking page</h1></div>
+
+    <form>
+      <FormGroup controlId="formControlsSelect">
+        <ControlLabel>Location</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="select">---</option>
+          <option value="location1">location 1</option>
+          <option value="location2">location 2</option>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup controlId="formControlsSelect">
+        <ControlLabel>Remaing spaces</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="select">---</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="more">3 or more</option>
+        </FormControl>
+      </FormGroup>
+
+      <Button block bsStyle="success" bsSize="large" type="submit">
+        Submit
+      </Button>
+    </form>
+  </div>
 )
 
 export default Parking
