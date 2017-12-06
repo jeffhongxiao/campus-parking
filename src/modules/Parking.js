e9ed11afd8cdeca294b771ea86dbaed106eb7859
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-12-05 20:57:35 -0400

    use class component; move function inside class

--- src/modules/Parking.js
+++ src/modules/Parking.js
@@ -2,39 +2,45 @@ import React from 'react'
 
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
-const submitForm = () => {
-  console.log('got here :)')
+
+
+class Parking extends React.Component {
+  submitForm() {
+    console.log('got here :)')
+  }
+
+  render() {
+    return (
+      <div>
+        <div><h1>Parking page</h1></div>
+
+        <form onSubmit={this.submitForm}>
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Location</ControlLabel>
+            <FormControl componentClass="select" placeholder="select">
+              <option value="select">---</option>
+              <option value="location1">location 1</option>
+              <option value="location2">location 2</option>
+            </FormControl>
+          </FormGroup>
+
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Remaing spaces</ControlLabel>
+            <FormControl componentClass="select" placeholder="select">
+              <option value="select">---</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="more">3 or more</option>
+            </FormControl>
+          </FormGroup>
+
+          <Button block bsStyle="success" bsSize="large" type="submit">
+            Submit
+          </Button>
+        </form>
+      </div>
+    )
+  }
 }
-
-const Parking = () => (
-  <div>
-    <div><h1>Parking page</h1></div>
-
-    <form onSubmit={submitForm}>
-      <FormGroup controlId="formControlsSelect">
-        <ControlLabel>Location</ControlLabel>
-        <FormControl componentClass="select" placeholder="select">
-          <option value="select">---</option>
-          <option value="location1">location 1</option>
-          <option value="location2">location 2</option>
-        </FormControl>
-      </FormGroup>
-
-      <FormGroup controlId="formControlsSelect">
-        <ControlLabel>Remaing spaces</ControlLabel>
-        <FormControl componentClass="select" placeholder="select">
-          <option value="select">---</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="more">3 or more</option>
-        </FormControl>
-      </FormGroup>
-
-      <Button block bsStyle="success" bsSize="large" type="submit">
-        Submit
-      </Button>
-    </form>
-  </div>
-)
 
 export default Parking
