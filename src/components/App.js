9b7c7c2b85e6b3c6e908a458ad3fd67aaf5c3932
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-12-20 21:34:22 -0400

    change react version; use Login.js from react-facebook-login-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Body from './Body'
 import Footer from './Footer'
+import Login from './Login'
 
 const App = () => (
   <div id='root'>
     <Body />
+    <Login />
     <Footer />
   </div>
 )
